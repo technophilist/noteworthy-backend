69d45f3156f2a1edb8ae91d8e22c9549bae8a9d1
technophilist <54663474+technophilist@users.noreply.github.com>
2024-06-29 09:18:17 -0400

    refactor: add auth-controller

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,69 @@
+import {Request, Response} from "express"
+import {authenticateUser, registerUser} from "../services/auth-service.js";
+import {areValidCredentials} from "../validations/auth-validations.js";
+
+/**
+ * Error message to be sent for internal server errors.
+ */
+const INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error."
+
+/**
+ * Registers a new user.
+ *
+ * @param {Request} req - The request object from Express, expected to contain user credentials in the body.
+ * @param {Response} res - The response object from Express.
+ */
+const registerNewUser = async (req: Request, res: Response) => {
+    const body = req.body
+
+    if (!areValidCredentials(body)) {
+        res.status(400).send({error: "Credentials missing."})
+        return
+    }
+
+    const email = body.email, password = body.password
+
+    try {
+        await registerUser(email, password)
+        res.status(200).send({success: "Successfully registered user."})
+    } catch (error) {
+        res.status(500).send({error: INTERNAL_SERVER_ERROR_MESSAGE})
+    }
+}
+
+/**
+ * Authenticates a user.
+ *
+ * @param {Request} req - The request object from Express, expected to contain user credentials in the body.
+ * @param {Response} res - The response object from Express.
+ */
+const login = async (req: Request, res: Response) => {
+    const body = req.body
+
+    if (!areValidCredentials(body)) {
+        res.status(400).send({error: "Credentials missing."})
+        return
+    }
+
+    const email = body.email, password = body.password
+    try {
+        const isUserSuccessfullyAuthenticated = await authenticateUser(email, password)
+        if (isUserSuccessfullyAuthenticated === null) {
+            res.status(401).send({error: "User with the specified email doesn't exist."})
+            return
+        }
+        if (isUserSuccessfullyAuthenticated) {
+            res.status(200).send({success: "User authenticated successfully."})
+            return
+        }
+        res.status(401).send({error: "Invalid credentials - email or password incorrect."})
+    } catch (error) {
+        res.status(500).send({error: INTERNAL_SERVER_ERROR_MESSAGE})
+    }
+}
+
+export {registerNewUser, login}
+
+
+
+
