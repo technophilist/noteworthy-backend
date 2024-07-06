7dd60f5bc3be8e36e61efda7fe84f286ef1d75fc
technophilist <54663474+technophilist@users.noreply.github.com>
2024-07-06 16:40:15 -0400

    fix: add support for .js imports in ts files for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1', // Support for .js imports in ts files
+    },
 };
