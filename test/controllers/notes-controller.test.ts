2f02d0c37ed95bb70faae8a002e5dae6dff21653
technophilist <54663474+technophilist@users.noreply.github.com>
2024-07-15 12:23:44 -0400

    test: add test for deleteNoteOfUserWithId & fix erroneous mock implementation

--- test/controllers/notes-controller.test.ts
+++ test/controllers/notes-controller.test.ts
@@ -1,7 +1,11 @@
 import {Request, Response} from "express"
 import {Note, NoteMetadata} from "../../src/models/notes/note";
 import {randomUUID} from "node:crypto";
-import {createNewNoteForUser, getAllNotesOfUserWithId} from "../../src/controllers/notes-controller";
+import {
+    createNewNoteForUser,
+    deleteNoteOfUserWithId,
+    getAllNotesOfUserWithId
+} from "../../src/controllers/notes-controller";
 
 
 jest.mock("../../src/services/notes-service", () => {
@@ -31,26 +35,34 @@ jest.mock("../../src/services/notes-service", () => {
         }
     ])
 
+    let nextNoteId = 1000
+
     return {
         createNewNote: async (note: Note) => {
-            let notesArray = []
-            if (map.has(note.associatedUserId)) {
-                notesArray = map.get(note.associatedUserId)!
-            } else {
-                map.set(note.associatedUserId, [])
-            }
+            let notesArray: Array<Note & NoteMetadata> = []
+            if (map.has(note.associatedUserId)) notesArray = map.get(note.associatedUserId)!
+            else map.set(note.associatedUserId, notesArray)
+
+            const noteId = nextNoteId++
             notesArray.push({
                 ...note,
-                noteId: randomUUID(),
+                noteId: noteId,
                 createdEpochTimestamp: String(Date.now()),
             })
+            return noteId
         },
         getAllNotesOfUser: async (userId: string) => map.get(userId) ?? [],
         updateNoteWithId: async () => {
             throw Error("Not implemented")
         },
-        deleteNoteWithId: async () => {
-            throw Error("Not implemented")
+        deleteNoteWithId: async (noteId: number) => {
+            for (const noteArray of map.values()) {
+                const note = noteArray.find((note) => note.noteId === noteId)
+                if (!note) continue
+                noteArray.splice(noteArray.indexOf(note), 1)
+                return true
+            }
+            return false
         },
         releaseResources: jest.fn()
     }
@@ -69,7 +81,7 @@ describe("notes-controller test", () => {
         }
 
         const statusSpy = jest.spyOn(response, "status")
-        
+
         await createNewNoteForUser(request as Request, response as Response)
         expect(statusSpy).toHaveBeenCalledWith(400)
     })
@@ -127,4 +139,81 @@ describe("notes-controller test", () => {
         expect(jsonSpy).not.toHaveBeenLastCalledWith([])
     })
 
+    test("Request to delete a note without a request body must return a response with status code set to 400", async () => {
+        const request: Partial<Request> = {}
+        const response: Partial<Response> = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        const statusSpy = jest.spyOn(response, "status")
+
+        await deleteNoteOfUserWithId(request as Request, response as Response)
+        expect(statusSpy).toHaveBeenCalledWith(400)
+    })
+
+    test("Request to delete a non-existent note must return a response with status code set to 404", async () => {
+        const request: Partial<Request> = {
+            query: {noteId: "non-existent note id"}
+        }
+        const response: Partial<Response> = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        const statusSpy = jest.spyOn(response, "status")
+
+        await deleteNoteOfUserWithId(request as Request, response as Response)
+        expect(statusSpy).toHaveBeenCalledWith(404)
+    })
+
+    test("Request to delete an existing note nust return a response with status code set to 200", async () => {
+
+        let testNoteId = ""
+        const createNoteRequest: Partial<Request> = {
+            body: {
+                userId: "userId",
+                title: "title",
+                content: "content"
+            }
+        }
+        const createNoteResponse: Partial<Response> = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockImplementation((obj) => testNoteId = String(obj.noteId))
+        }
+
+        const createNoteStatusSpy = jest.spyOn(createNoteResponse, "status")
+
+        await createNewNoteForUser(createNoteRequest as Request, createNoteResponse as Response)
+        expect(createNoteStatusSpy).toHaveBeenCalledWith(200)
+
+        const request: Partial<Request> = {
+            query: {noteId: testNoteId}
+        }
+        const response: Partial<Response> = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        const statusSpy = jest.spyOn(response, "status")
+
+        await deleteNoteOfUserWithId(request as Request, response as Response)
+        expect(statusSpy).toHaveBeenCalledWith(200)
+    })
+
+    test("Request to delete an non-existing note nust return a response with status code set to 404", async () => {
+
+        const request: Partial<Request> = {
+            query: {noteId: "123123"}
+        }
+        const response: Partial<Response> = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        const statusSpy = jest.spyOn(response, "status")
+
+        await deleteNoteOfUserWithId(request as Request, response as Response)
+        expect(statusSpy).toHaveBeenCalledWith(404)
+    })
 })
